06c94492847e169b59fa0c0a2c4b32da8e35b8ed
rstevenn <romain.stevenne@student.unamur.be>
2023-12-11 14:33:10 +0100

    implement rng interface for linux

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include "chacha.h"
 
@@ -39,6 +40,20 @@ void secure_rand(uint8_t* out, size_t size)
     }
 }
 #else
+
+void secure_rand(uint8_t* out, size_t size) {
+    FILE* fp = fopen("/dev/urandom", "rb");
+    if (fp == NULL) {
+        printf("Can't access random générator\n");
+        exit(1);
+    }
+
+    if (fread(out, sizeof(*out), size, fp) != size) {
+        printf("Can't generate random bytes\n");
+        exit(1);
+    }
+    fclose(fp);
+}
 
 #endif
 
